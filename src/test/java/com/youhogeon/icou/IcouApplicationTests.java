529f2e7acb716bb95888061c9b40031752625d9c
YouHoGeon <me@youhogeon.com>
2022-12-19 19:34:05 +0900

    first commit

---
+++ src/test/java/com/youhogeon/icou/IcouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youhogeon.icou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
